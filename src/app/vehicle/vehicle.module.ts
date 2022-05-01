cdbc7db000abcaf7295e61587d488d55ea4fbc14
hawerforerouniandes <h.foreror@uniandes.edu.co>
2022-05-01 16:25:04 -0500

    Add vista lista vehiculos

--- src/app/vehicle/vehicle.module.ts
+++ src/app/vehicle/vehicle.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { VehicleComponent } from './vehicle.component';
+import { VehicleRoutingModule } from './vehicle-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { VehicleListComponent } from './vehicle-list/vehicle-list.component';
 
 @NgModule({
   imports: [
-    CommonModule
-  ],
-  declarations: [VehicleComponent]
+    CommonModule,
+    VehicleRoutingModule  ],
+  declarations: [VehicleListComponent]
 })
 export class VehicleModule { }
