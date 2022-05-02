9d6af695618f619e04ad810e8943ca9f22bcfcae
hawerforerouniandes <h.foreror@uniandes.edu.co>
2022-05-01 20:21:45 -0500

    Calcula los totales por marca dinamicamnte

--- src/app/vehicle/vehicle-list/vehicle-list.component.ts
+++ src/app/vehicle/vehicle-list/vehicle-list.component.ts
@@ -10,19 +10,33 @@ import { VehicleService } from '../vehicle.service';
 export class VehicleListComponent implements OnInit {
 
   vehicles: Array<Vehicle> = [];
-  marcas: Array<string> = [];
-  valores: Array<number> = [];
-
+  marcas: Array<any> = [];
+  valores: any = {};
   constructor(private vehicleService: VehicleService) { }
 
   getVehicles(): void {
     this.vehicleService.getVehicles().subscribe((vehicles) => {
       this.vehicles = vehicles;
+      for (let elemento of this.vehicles) {
+
+        var include = this.marcas.includes(elemento.marca)
+        if(!include){
+          this.marcas.push(elemento.marca)
+          this.valores[elemento.marca] = 1;
+        }
+        else{
+          this.valores[elemento.marca] = this.valores[elemento.marca] + 1;
+        }
+      }
+      console.log(this.marcas)
+      console.log(this.valores)
     });
+
   }
 
   ngOnInit() {
     this.getVehicles();
+
 
   }
 
