c1da76b04a559a776e27356083ddf29438d77ec3
hawerforerouniandes <h.foreror@uniandes.edu.co>
2022-05-01 21:01:30 -0500

    Prueba de componente list

--- src/app/vehicle/vehicle-list/vehicle-list.component.spec.ts
+++ src/app/vehicle/vehicle-list/vehicle-list.component.spec.ts
@@ -2,16 +2,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { VehicleListComponent } from './vehicle-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { VehicleService } from '../vehicle.service';
+import { Vehicle } from '../vehicle';
 
 describe('VehicleListComponent', () => {
   let component: VehicleListComponent;
   let fixture: ComponentFixture<VehicleListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ VehicleListComponent ]
+      imports: [HttpClientModule],
+      declarations: [ VehicleListComponent ],
+      providers: [ VehicleService ]
     })
     .compileComponents();
   }));
@@ -19,10 +26,54 @@ describe('VehicleListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(VehicleListComponent);
     component = fixture.componentInstance;
+
+    component.vehicles = [
+      new Vehicle(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        "",
+        "",
+        "",
+        "" ),
+      new Vehicle(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        "",
+        "",
+        "",
+        "" ),
+      new Vehicle(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        "",
+        "",
+        "",
+        "" ),
+    ];
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
+
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('verificar vehiculos creados ', () => {
+
+    const compiled = fixture.debugElement.nativeElement;
+    var esperado = "#MarcaLíneaModelo"
+    for (let elemento of component.vehicles) {
+      esperado = esperado + elemento.id+elemento.marca+elemento.linea+elemento.modelo;
+    }
+    expect(compiled.querySelector('.table').textContent).toEqual(esperado);
+
   });
 });
--- src/app/vehicle/vehicle.ts
+++ src/app/vehicle/vehicle.ts
@@ -3,8 +3,8 @@ export class Vehicle {
   id: number;
   marca: string;
   linea: string;
-  referencia: string;
   modelo: string;
+  referencia: string;
   kilometraje: string;
   color: string;
   imagen: string;
@@ -13,8 +13,8 @@ export class Vehicle {
     id: number,
     marca: string,
     linea: string,
-    referencia: string,
     modelo: string,
+    referencia: string,
     kilometraje: string,
     color: string,
     imagen: string,
@@ -23,8 +23,8 @@ export class Vehicle {
     this.id = id;
     this.marca = marca;
     this.linea = linea;
-    this.referencia = referencia;
     this.modelo = modelo;
+    this.referencia = referencia;
     this.kilometraje = kilometraje;
     this.color = color;
     this.imagen = imagen;
